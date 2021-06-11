15bd559b664561a07b9b0b0b38a483d3dfc268b7
RandneyHancis <hancis_666@hotmail.com>
2021-06-11 08:42:30 -0300

    Incremento do IdJogador na classe jogador

--- src/main/java/com/example/demo/cardpack/Jogador.java
+++ src/main/java/com/example/demo/cardpack/Jogador.java
@@ -1,5 +1,52 @@
 package com.example.demo.cardpack;
 
-public class Jogador {
+import javax.swing.JLabel;
 
+/**
+*
+* @author Demerson, Paulo, Randney
+*/
+public class Jogador {
+   private final int cartasNoJogo = 5;
+   private final String nomeJogador;
+   private CartaBaralho[] mao = CartaBaralho.geraBaralho();
+   private int pontos;
+   private int idJogador;
+
+   public Jogador(String nomeJogador, CartaBaralho[] mao, int pontos) {
+       this.nomeJogador = nomeJogador;
+       this.mao = CartaBaralho.geraBaralho();
+       this.pontos = 0;
+   }
+
+   public int getCartasNoJogo() {
+       return cartasNoJogo;
+   }
+
+   public String getNomeJogador() {
+       return nomeJogador;
+   }
+
+   public CartaBaralho[] getMao() {
+       return mao;
+   }    
+
+   public int getPontos() {
+       return pontos;
+   }
+         
+   public int soma(int i, int j, Jogador maoJogador, Jogador maoCasa, int soma, JLabel pontos) {
+       soma += maoJogador.getMao()[j].getCarta().getPontos() + maoCasa.getMao()[i].getCarta().getPontos();            
+       pontos.setText(Integer.toString(soma));        
+       return soma;
+   }
+
+   public int getIdJogador() {
+	   return idJogador;
+   }
+
+   public void setIdJogador(int idJogador) {
+	   this.idJogador = idJogador;
+   }
+   
 }
